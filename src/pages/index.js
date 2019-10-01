5cf1c0baf1f992b4961f747932e7f7740f375df1
Kenia Valladarez <keniavalladarez@gmail.com>
2019-09-30 21:02:57 -0400

    Edit site title

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,7 +11,7 @@ function index() {
       <Info />
       <Helmet>
         <meta charSet="utf-8" />
-        <title>Home | Sumanth</title>
+        <title>Kenia</title>
       </Helmet>
     </div>
   )
