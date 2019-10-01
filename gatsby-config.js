d8bd34f3afffe57c6950222f59e32d3e1aeaff73
Kenia Valladarez <keniavalladarez@gmail.com>
2019-09-30 20:44:06 -0400

    Install new gatsby template

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Kenia`,
+    description: `Kenia's Website`,
+    author: `Kenia`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -38,6 +38,5 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
-    
   ],
 }
--- src/components/info.js
+++ src/components/info.js
@@ -3,8 +3,6 @@ import React from "react"
 import {
   FaTwitter as Twitter,
   FaGithub as Github,
-  FaYoutube as Youtube,
-  FaInstagram as Insta,
   FaEnvelope as Mail,
 } from "react-icons/fa"
 
@@ -13,34 +11,22 @@ export default function info() {
     <div className="container">
       <div className="my-5"></div>
       <div className="display-3" style={{ color: "#FF8A00" }}>
-        <span style = {{color : "white"}}>Hello, I'm</span> <br />
-        Sumanth.
+        <span style={{ color: "white" }}>Hello, I'm</span> <br />
+        Kenia.
       </div>
       <div className="h1 code mt-4 mb-3">async {"{"}</div>
       <div className="text-muted mx-5 my-4 h3 text-justify info">
-        A Passionate Front-end Developer, UI/UX Enthusiast, Android Geek and Web
-        Developer.
-        <br /> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-        eiusmod tempor incididunt ut labore et.
+        UI Engineer
       </div>
       <div className="h1 code mt-2 mb-3">{"}"}</div>
       <div className="h1 mt-5">
-        <a className="mr-5 icon" href="https://twitter.com/Mr404Found">
+        <a className="mr-5 icon" href="https://twitter.com/kaxcode">
           <Twitter />
         </a>
-        <a className="mr-5 icon" href="https://instagram.com/sumanth.zero7">
-          <Insta />
-        </a>
-        <a className="mr-5 icon" href="https://github.com/Mr404Found">
+        <a className="mr-5 icon" href="https://github.com/kaxcode">
           <Github />
         </a>
-        <a
-          className="mr-5 icon"
-          href="https://www.youtube.com/channel/UCw0FDIL-HpcfzEZrjcQwhMQ"
-        >
-          <Youtube />
-        </a>
-        <a className="mr-5 icon" href="mailto://contactmr404here@gmail.com">
+        <a className="mr-5 icon" href="mailto://keniavalladarez@gmail.com">
           <Mail />
         </a>
       </div>
